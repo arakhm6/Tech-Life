38abad584d105c0d5893c9e2e0c3944b1d874a4c
arakhm6 <arakhm6@gmail.com>
2021-03-04 06:27:14 -0500

    toggle burger

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
-let mouse;
+
 
 function animateSlides()
 {
@@ -48,9 +48,10 @@ function animateSlides()
         });
 }
 
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
 function cursor(e){
-    mouse = document.querySelector('.cursor');
-    let mouseTxt = mouse.querySelector('span');
     mouse.style.top = e.pageY + 'px';
     mouse.style.left = e.pageX + 'px';
 }
@@ -79,6 +80,29 @@ function activeCursor(e){
     }
 }
 
+function navToggle(e)
+{
+    if(!e.target.classList.contains('active'))
+    {
+        gsap.to('.line1', 0.5, {rotate: '45', y:5, background: 'black'});
+        gsap.to('.line2', 0.5, {rotate: '-45', y:-5, background: 'black'});
+        gsap.to('.#logo', 1, {color: 'black'});
+        gsap.to('.nav-bar', 1, {clipPath: 'cirlce(2500px at 100% - 10%)'});
+        document.body.classList.add('hide');
+    }
+    else
+    {
+        e.target.classList.contains('active');
+        gsap.to('.line1', 0.5, {rotate: '0', y:0, background: 'white'});
+        gsap.to('.line2', 0.5, {rotate: '0', y:0, background: 'white'});
+        gsap.to('.#logo', 1, {color: 'white'});
+        gsap.to('.nav-bar', 1, {clipPath: 'cirlce(50px at 100% - 10%)'});
+        document.body.classList.remove('hide');
+    }
+}
+
+//Eventlisteners
+burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 animateSlides(); 
